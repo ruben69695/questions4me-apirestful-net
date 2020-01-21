a8eef9fe6dfa73ad2dc1b36784f9ff47cc2bba6f
Ruben Arrebola <ruben.arre6@gmail.com>
2020-01-21 20:39:02 +0100

    Changed question model, changed json property names

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace questions4me_apirestful_net.Models
 {
     public class Question
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("created_by")]
         public string CreatedBy { get; set; }
+
+        [JsonPropertyName("content")]
         public string Content { get; set; }
+
+        [JsonPropertyName("created_at")]
         public DateTime CreatedAt { get; set; }
+        
+        [JsonPropertyName("answered_at")]
         public DateTime? AnsweredAt { get; set; }
     }
 }
